724bd4fe9eea83159995005c7a517373fe6c914b
Vladislav Mishukov <58140946+vmishukov@users.noreply.github.com>
2020-10-21 15:41:04 +0700

    nav fix

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
 <head>
     <meta charset="utf-8">
     <title>Blog</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <?php
     if(!isset($_SESSION['username'])){
         echo "<a class='navbar-brand' href='login.php'>Log in!</a>";
@@ -20,16 +25,17 @@
         $user_id = $_SESSION['id'];
         $user_name = mysqli_fetch_assoc(mysqli_query($db,"SELECT username FROM users WHERE id='$user_id'"))['username'] or die(mysqli_error($db));
         echo "<a class='navbar-brand' >Welcome, $user_name!</a>";
-        echo "
-                <ul class='navbar-nav mr-auto'>    
-                   
-                    <li class='nav-item active'>
-                        <a class='nav-link' class='nav-link' href='post.php'>Post!</a>
-                    </li>
-                     <li class='nav-item'>              
-                        <a class='nav-link' href='logout.php'>Log out</a>
-                    </li>
-                </ul>";
+
+        echo "<div class='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <ul class='navbar-nav'>                       
+                        <li class='nav-item active '>
+                            <a class='nav-link' class='nav-link' href='post.php'>Post!</a>
+                        </li>
+                         <li class='nav-item'>              
+                            <a class='nav-link' href='logout.php'>Log out</a>
+                        </li>
+                    </ul>
+                </div>";
     }
 
     ?>
@@ -56,7 +62,7 @@
                     $admin ="";
                 }
                 $posts .= "
-                <div class='card' >
+                <div class='card fluid'>
                     <div class='card-body col-5'>
                         <h5 class='card-title'><a href='view_post.php?pid=$id' target='_blank'>$title By $user_name</a></h5>
                             
--- view_post.php
+++ view_post.php
@@ -23,13 +23,17 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <title>Blog</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <?php
         if(!isset($_SESSION['username'])){
             echo "<a class='navbar-brand' href='login.php'>Log in!</a>
@@ -44,7 +48,7 @@
             $user_id = $_SESSION['id'];
             $user_name = mysqli_fetch_assoc(mysqli_query($db,"SELECT username FROM users WHERE id='$user_id'"))['username'] or die(mysqli_error($db));
             echo "<a class='navbar-brand' >Welcome, $user_name!</a>";
-            echo "
+            echo "<div class='collapse navbar-collapse' id='navbarSupportedContent'>
                     <ul class='navbar-nav mr-auto'>                         
                         <li class='nav-item active'>
                             <a class='nav-link' class='nav-link' href='index.php'>Home</a>
@@ -52,7 +56,8 @@
                          <li class='nav-item'>              
                             <a class='nav-link' href='logout.php'>Log out</a>
                         </li>
-                    </ul>";
+                    </ul>
+                    </div>";
         }
         ?>
     </nav>
